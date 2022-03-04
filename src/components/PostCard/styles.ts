671eb1472a62f40f5c29b3657d521bb7004d7d54
Thales Fernando <thalesf.galvao@gmail.com>
2022-03-04 10:31:26 -0300

    Feat: create post card, use axios api and create capitalize letter

---
+++ src/components/PostCard/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: 2fr 4fr 4fr;
+  grid-template-columns: 4fr 4fr;
+  background: linear-gradient(
+    152.97deg,
+    rgba(255, 255, 255, 0.2) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  backdrop-filter: blur(1rem);
+  /* Note: backdrop-filter has minimal browser support */
+  border-radius: 1.5rem;
+  padding: 2rem 5rem;
+  margin-top: 3rem;
+  width: 45vh;
+  height: 50vh;
+`
+
+export const PokemonName = styled.h1`
+  grid-row: 1;
+  grid-column: 1/4;
+  text-align: center;
+`
+
+export const PokemonSprite = styled.img`
+  grid-row: 2;
+  grid-column: 1/4;
+  align-self: center;
+`
+
+export const LeftInformation = styled.div`
+  display: flex;
+  gap: 5rem;
+  flex-direction: column;
+  grid-column: 1/2;
+  grid-row: 3;
+  gap: 0.2rem;
+  font-weight: bold;
+`
+
+export const Rightinformation = styled.div`
+  display: flex;
+  gap: 5rem;
+  flex-direction: column;
+  grid-column: 3/4;
+  grid-row: 3;
+  gap: 0.2rem;
+  font-weight: bold;
+`
+
+export const PokemonInformation = styled.p`
+  font-weight: lighter;
+`
